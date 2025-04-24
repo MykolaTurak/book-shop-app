a1f3e86a3610f5e7ebc287b83c39102603cad968
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-04-25 00:22:35 +0300

    solve remarked problems

--- src/main/java/mate/academy/demo/controler/AuthenticationController.java
+++ src/main/java/mate/academy/demo/controler/AuthenticationController.java
@@ -1,5 +1,7 @@
 package mate.academy.demo.controler;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import mate.academy.demo.dto.user.UserRegistrationRequestDto;
 import mate.academy.demo.dto.user.UserResponseDto;
@@ -11,12 +13,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "Authentication management", description = "Endpoints for authentication")
 @RestController
 @RequestMapping("/auth")
 public class AuthenticationController {
     @Autowired
     private UserService userService;
 
+    @Operation(summary = "Register user", description = "Register user and add him to db")
     @PostMapping("/registration")
     public UserResponseDto register(@Valid @RequestBody UserRegistrationRequestDto request)
             throws RegistrationException {
