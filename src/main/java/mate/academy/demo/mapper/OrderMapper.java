585fb388352301cff0645ab5a71ec8ff61f0a886
Mykola <mykola.turak.ki.2023@lpnu.ua>
2025-07-10 14:13:43 +0300

    fixed order mapper bug

--- src/main/java/mate/academy/demo/mapper/OrderMapper.java
+++ src/main/java/mate/academy/demo/mapper/OrderMapper.java
@@ -5,9 +5,11 @@ import mate.academy.demo.dto.order.OrderCreateRequestDto;
 import mate.academy.demo.dto.order.OrderDto;
 import mate.academy.demo.model.Order;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(config = MapperConfig.class, uses = OrderItemMapper.class)
 public interface OrderMapper {
+    @Mapping(target = "userId", source = "user.id")
     OrderDto toDto(Order order);
 
     Order toModel(OrderCreateRequestDto orderCreateRequestDto);
